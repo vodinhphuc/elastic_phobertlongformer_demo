8dd3048eb2f28cd04c546dd9bd9fb8e76e6d02a5
vodinhphuc <phucvd2512@gmail.com>
2021-07-28 11:28:54 +0700

    chore: auto coppy .env file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+cp .env_example .env
+
+mkdir -p data
 mkdir -p vncorenlp/models/wordsegmenter
+
 wget -q --show-progress https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/VnCoreNLP-1.1.1.jar -P vncorenlp
 wget -q --show-progress https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/wordsegmenter/vi-vocab -P vncorenlp/models/wordsegmenter/
 wget -q --show-progress https://raw.githubusercontent.com/vncorenlp/VnCoreNLP/master/models/wordsegmenter/wordsegmenter.rdr -P vncorenlp/models/wordsegmenter/
